ee6c39720414723fec74d0b10a0f9e7ee966381f
Ingo Sauerzapf <isauerzapf@snaplogic.com>
2024-11-09 02:44:48 +0000

    changed workspace directory

--- scripts/install-jcc.sh
+++ scripts/install-jcc.sh
@@ -10,7 +10,6 @@ chmod 600 ~/opt/snaplogic/etc/*.slpropz
 rm $snapfile
 rm -r ~/usr
 echo 'export SL_ROOT=$HOME/opt/snaplogic' >> ~/.bash_profile
-echo 'export SNAP_HOME=/workplaces/snap-dev' >> ~/.bash_profile
+echo 'export SNAP_HOME=/workspaces/{$1}' >> ~/.bash_profile
 echo 'export JCC_DEBUG_PORT=8827' >> ~/.bash_profile
-source ~/.bash_profile
 echo "JCC is install and can be run by executing: cd ~/opt/snaplogic/run/lib && java -jar jcc.war jcc"
